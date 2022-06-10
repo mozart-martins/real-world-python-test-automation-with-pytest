f26cac4227544296aeb0df151dba67ca49e0544b
Mozart Dias Martins <mozart.dias.martins@gmail.com>
2022-06-09 21:33:12 -0300

    Criada a api de company

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .serializers import CompanySerializer
+from .models import Company
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
+    pagination_class = PageNumberPagination
