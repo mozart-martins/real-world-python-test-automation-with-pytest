a0642c0bed62bd66a608a928f1aa4ed5b9ff753f
Mozart Dias Martins <mozart.dias.martins@gmail.com>
2022-06-29 10:37:06 -0300

    Marca api

--- api/coronavstech/fibonacci/tests/test_fib.py
+++ api/coronavstech/fibonacci/tests/test_fib.py
@@ -13,18 +13,21 @@ fibonacci_url = reverse('fib-seq')
 def test_naive(time_tracker, fib_func: Callable[[int], int], n: int, expected: int) -> None:
     assert fib_func(n) == expected
     
+# eh apenas uma marca... nao eh necessari
 @pytest.mark.api
 def test_no_n_parameter_should_return_response(client) -> None:
     response = client.get(fibonacci_url)
     assert response.status_code == 200
     assert response.json() == "Hello there, no number was passed"
 
+# eh apenas uma marca... nao eh necessari
 @pytest.mark.api
 def test_post_method_with_fib_endpoint_should_fail(client) -> None:
     response = client.post(fibonacci_url, data={"n": 4})
     assert response.status_code == 405
     assert response.json()["detail"] == 'Method "POST" not allowed.'
 
+# eh apenas uma marca... nao eh necessari
 @pytest.mark.api
 @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_stress_fibonacci_endpoint(client, n: int, expected: int) -> None:
